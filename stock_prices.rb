584eea6f95553efb5c4382e389870c93e3e553b0
Zakry Zapata <zakry.zapata@gmail.com>
2015-08-01 15:17:31 -0500

    added psuedo code for method

---
+++ stock_prices.rb
@@ -0,0 +1,14 @@
+def stock_picker (prices)
+# Set default max profit to 0
+# Set default buying price to 0 and buying index to 0
+# default selling price = 0 and selling index to 0
+# For each value in the prices array
+	# If current value minus next value is less than max profit
+		# max profit = current value minus next value
+		# buying price = current value
+		# buying index = current value index
+		# selling price = next value
+		# selling index = next value index
+	# Else
+		# continue loop
+end
